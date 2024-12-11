613e19f7c28060a9ee1886cf24c325509e6f1d61
Aditya kumar mishra <157035985+Aditya41205@users.noreply.github.com>
2024-12-11 12:43:49 +0530

    updated

---
+++ .dist/Calculator.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+    float a,b,sum,difference,multiply,divide;
+    printf("Enter two numbers:");
+    scanf("%d%d",&a,&b);
+
+    printf("Enter the operator to perform operation:");
+
+    if()
+
+    
+}
