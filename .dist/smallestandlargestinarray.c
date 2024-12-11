651410b58d026fbc6410bdea7427559130866d2c
Aditya kumar mishra <157035985+Aditya41205@users.noreply.github.com>
2024-12-11 12:30:05 +0530

    updated

---
+++ .dist/smallestandlargestinarray.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(){
+    int n,i,smallest,largest;
+
+    printf("the size of 1d array is :");
+    scanf("%d",&n);
+
+    int arr[n];
+
+    printf("Enter %d elements in array :",n);
+    for(i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+
+smallest=largest=arr[0];
+
+for(i=1;i<n;i++){
+    if(arr[i]<smallest){
+        smallest=arr[i];
+    }
+}
+
+for(i=1;i<n;i++){
+    if(arr[i]>largest){
+        largest=arr[i];
+    }
+}
+
+printf("largest number in array is %d\n",largest);
+printf("largest number in array is %d\n",smallest);
+
+return 0;
+
+}
