4b798fc29d4506431e204b3cfd75fd3b4b3dfc1b
Aditya41205 <157035985+Aditya41205@users.noreply.github.com>
2024-12-14 13:13:54 +0530

    updaated

--- .dist/Calculator.c
+++ .dist/Calculator.c
@@ -1,13 +1,41 @@
 #include <stdio.h>
 
-int main(){
-    float a,b,sum,difference,multiply,divide;
-    printf("Enter two numbers:");
-    scanf("%d%d",&a,&b);
+int main() {
+  float num1, num2, result;
+  char operator;
 
-    printf("Enter the operator to perform operation:");
+  printf("Enter the first number: ");
+  scanf("%f", &num1);
 
-    if()
+  printf("Enter the second number: ");
+  scanf("%f", &num2);
 
-    
+  printf("Enter the operator (+, -, *, /): ");
+  scanf(" %c", &operator);
+
+  switch(operator) {
+    case '+':
+      result = num1 + num2;
+      break;
+    case '-':
+      result = num1 - num2;
+      break;
+    case '*':
+      result = num1 * num2;
+      break;
+    case '/':
+      if (num2 == 0) {
+        printf("Error: Division by zero\n");
+        return 1; 
+      }
+      result = num1 / num2;
+      break;
+    default:
+      printf("Invalid operator\n");
+      return 1; 
+  }
+
+  printf("Result: %.2f\n", result);
+
+  return 0;
 }
